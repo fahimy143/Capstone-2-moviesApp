1ed1c8fce3b5810b68239b59f5fbf86d96f170d0
Mustafa Fahimy <57341400+fahimy143@users.noreply.github.com>
2022-07-28 00:26:23 +0430

    change the files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import { getMovies, searchbar } from './models/displayMovies';
-
-const form = document.getElementById('form');
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d2f3f7df7854008b58687802ab337feb&page=1';
-
-getMovies(API_URL);
-form.addEventListener('submit', searchbar());
